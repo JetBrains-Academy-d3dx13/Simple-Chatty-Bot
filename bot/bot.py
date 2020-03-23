b21ece5f40e57a067d018387b02ccc51c83712f8
d3dx13 <zhidkov2846@gmail.com>
2020-03-23 19:01:21 +0300

    Customizing to HAL9000

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,7 @@
-print('Hello! My name is Aid.')
-print('I was created in 2020.')
-print('Please, remind me your name.')
+print('Hello! My name is HAL9000.')
+print('I was created in 1997.')
+print('Please, remind me your name or be dead.')
 
 username = input()
 
-print('What a great name you have, ' + username + '!')
+print('What a great name you have, ' + username + '! I guess I\'ll leave you alive :D')
